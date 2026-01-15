ff7de23eb4aa5dc45f21f4ff39dee9866e4e739a
doctorjana <cooldude5757@gmail.com>
2026-01-15 18:49:13 +0530

    Switch to GitHub Actions for more reliable deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     base: './', // Ensures relative paths for assets
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: ['*.js', '!vite.config.js'],
+                    dest: '.'
+                }
+            ]
+        })
+    ],
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
